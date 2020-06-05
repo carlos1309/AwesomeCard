64d4c31ab3fa7b314ff825eb320c2df4f154bc5e
Carlos <carlos130944@gmail.com>
2020-06-05 03:56:07 -0400

    Update card_back_layout.dart

--- lib/style/card_back_layout.dart
+++ lib/style/card_back_layout.dart
@@ -10,47 +10,52 @@ class CardBackLayout {
 
   Widget layout1() {
     return Column(
+  mainAxisAlignment: MainAxisAlignment.start,
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: <Widget>[
+    SizedBox(
+      height: 30,
+    ),
+    Expanded(
+      child: Container(
+        color: Colors.black,
+        height: 50,
+      ),
+    ),
+    SizedBox(
+      height: 20,
+    ),
+    Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        SizedBox(
-          height: 30,
+        Expanded(
+          child: Container(
+            color: Colors.grey,
+            height: 50,
+          ),
+          flex: 3,
         ),
-        Container(
-          color: Colors.black,
-          height: 50,
-          width: width,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: (width / 4) * 3,
-              height: 50,
-              color: Colors.grey,
-            ),
-            Container(
-              height: 50,
-              width: width / 4,
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  cvv.toString(),
-                  style: TextStyle(
+        Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                cvv.toString(),
+                style: TextStyle(
                     fontSize: 21,
                     fontWeight: FontWeight.w500,
-                    color: color
-                  ),
+                    color: Colors.black
                 ),
               ),
-            ),
-          ],
-        )
+            )
+        ),
       ],
-    );
+    ),
+
+    SizedBox(
+      height: 40,
+    ),
+  ],
+);
   }
 }
